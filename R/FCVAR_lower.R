cdd5801037b42c03fc098af30e7377c1beaecf80
Lee Morin <Lealand.Morin@ucf.edu>
2020-01-12 00:40:12 -0500

    Translated calculation of the Hessian matrix

--- R/FCVAR_lower.R
+++ R/FCVAR_lower.R
@@ -319,7 +319,7 @@ FCVARlikeMu <- function(y, db, mu, k, r, opt) {
   
   
   t <- nrow(y)
-  x <- y - matrix(1, nrow <- t, ncol <- 1) %*% mu
+  x <- y - matrix(1, nrow = t, ncol = 1) %*% mu
   
   # Obtain concentrated parameter estimates.
   estimates <- GetParams(x, k, r, db, opt)
@@ -392,7 +392,7 @@ FCVARlike <- function(x, params, k, r, opt) {
   
   # Modify the data by a mu level.
   if (opt$levelParam) {
-    y <- x - matrix(1, nrow <- T, ncol <- p) %*% diag(mu)
+    y <- x - matrix(1, nrow = T, ncol = p) %*% diag(mu)
   }
   else {
     y <- x
@@ -678,17 +678,109 @@ FreeParams <- function(k, r, p, opt, rankJ) {
 }
 
 
+################################################################################
+# Define function to calculate the Hessian matrix. 
+################################################################################
+# 
+# function [ hessian ] <- FCVARhess(x, k, r, coeffs, opt)
+# Written by Michal Popiel and Morten Nielsen (This version 10.22.2014)
+# 
+# DESCRIPTION: This function calculates the Hessian matrix of the
+# 	log-likelihood numerically.
+# 
+# Input <- x (matrix of variables to be included in the system)
+#         k (number of lags)
+#         r (number of cointegrating vectors)
+#         coeffs (coefficient estimates around which estimation takes place)
+#         opt (object containing the estimation options)
+# Output <- hessian (matrix of second derivatives)
+# 
+################################################################################
+
+
+
+FCVARhess <- function(x, k, r, coeffs, opt) {
+  
+  
+  # Set dimensions of matrices.
+  p <- ncol(x)
+  
+  # rhoHat and betaHat are not used in the Hessian calculation.
+  rho <- coeffs$rhoHat
+  beta <- coeffs$betaHat
+  
+  # Specify delta (increment for numerical derivatives).
+  delta <- 10^(-4)
+  
+  # Convert the parameters to vector form.
+  phi0 <- SEmat2vecU(coeffs, k, r, p, opt)
+  
+  # Calculate vector of increments.
+  deltaPhi <- delta*matrix(1, nrow = nrow(phi0), ncol = ncol(phi0))
+  
+  nPhi <- length(phi0)
+  
+  # Initialize the Hessian matrix.
+  hessian <- matrix(0, nrow = nPhi, ncol = nPhi)
+  
+  # The loops below evaluate the likelihood at second order incremental
+  #   shifts in the parameters. 
+  
+  
+  for (i in 1:nPhi) {
+    
+    
+    for (j in 1:i) {
+      
+      
+      # positive shift in both parameters.
+      phi1 <- phi0 + deltaPhi*( (1:nPhi) == i ) + deltaPhi*( (1:nPhi) == j )
+      coeffsAdj <- SEvec2matU( phi1, k, r, p, opt )
+      # calculate likelihood
+      like1 <- FullFCVARlike(x, k, r, coeffsAdj, beta, rho, opt )
+      
+      # negative shift in first parameter, positive shift in second
+      # parameter. If same parameter, no shift.
+      phi2 <- phi0 - deltaPhi*( (1:nPhi) == i ) + deltaPhi*( (1:nPhi) == j )
+      coeffsAdj <- SEvec2matU( phi2, k, r, p, opt )
+      # calculate likelihood
+      like2 <- FullFCVARlike(x, k, r, coeffsAdj, beta, rho, opt)
+      
+      # positive shift in first parameter, negative shift in second
+      # parameter. If same parameter, no shift.
+      phi3 <- phi0 + deltaPhi*( (1:nPhi) == i ) - deltaPhi*( (1:nPhi) == j )
+      coeffsAdj <- SEvec2matU( phi3, k, r, p, opt )
+      # calculate likelihood
+      like3 <- FullFCVARlike(x, k, r, coeffsAdj, beta, rho, opt)
+      
+      # negative shift in both parameters.
+      phi4 <- phi0 - deltaPhi*( (1:nPhi) == i ) - deltaPhi*( (1:nPhi) == j )
+      coeffsAdj <- SEvec2matU( phi4, k, r, p, opt )
+      # calculate likelihood
+      like4 <- FullFCVARlike(x, k, r, coeffsAdj, beta, rho, opt)
+      
+      # The numerical approximation to the second derivative.
+      hessian[i,j] <- ( like1 - like2 - like3 + like4 )/4/deltaPhi[i]/deltaPhi[j]
+      
+      # Hessian is symmetric.
+      hessian[j,i] <- hessian[i,j]
+      
+      
+    }
+    
+  }
+  
+  return(hessian)
+}
+
+
+
 
 
 
 # Functions to make: 
 
 
-# fp <- FreeParams(k, r, p, opt, rankJ)
-
-
-# Calculate unrestricted Hessian.
-# H <- FCVARhess(x, k, r, results$coeffs, opt)
 
 # SEmat2vecU(results$coeffs, k, r, p, opt)
 # SEvec2matU(SE,k,r,p, opt)
--- R/FCVAR_scratchpad.R
+++ R/FCVAR_scratchpad.R
@@ -2,55 +2,81 @@
 
 # Temporary staging area for Matlab code. 
 
-
-
-function [ fp ] <- FreeParams(k, r, p, opt, rankJ)
-# function [ fp ] <- FreeParams(k, r, p, opt, rankJ)
-# Written by Michal Popiel and Morten Nielsen (This version 05.22.2015)
+function [ hessian ] <- FCVARhess(x, k, r, coeffs, opt)
+# function [ hessian ] <- FCVARhess(x, k, r, coeffs, opt)
+# Written by Michal Popiel and Morten Nielsen (This version 10.22.2014)
 # 
-# DESCRIPTION: This function counts the number of free parameters based on
-# 	the number of coefficients to estimate minus the total number of
-# 	restrictions. When both alpha and beta are restricted, the rank condition
-# 	is used to count the free parameters in those two variables.
-%
+# DESCRIPTION: This function calculates the Hessian matrix of the
+# 	log-likelihood numerically.
+# 
 # Input <- x (matrix of variables to be included in the system)
 #         k (number of lags)
 #         r (number of cointegrating vectors)
+#         coeffs (coefficient estimates around which estimation takes place)
 #         opt (object containing the estimation options)
-# Output <- fp (number of free parameters)
-%_________________________________________________________________________
+# Output <- hessian (matrix of second derivatives)
 
 
-# ---- First count the number of parameters -------- %
-  fDB <- 2 # updated by rDB below for the model d=b (1 fractional parameter)
-  fpA <- p*r # alpha
-  fpB <- p*r # beta
-  fpG <- p*p*k # Gamma
-  fpM <- p*opt$levelParam # mu
-  fpRrh <- r*opt$rConstant # restricted constant
-  fpUrh <- p*opt$unrConstant # unrestricted constant
-  
-  numParams <- fDB + fpA + fpB + fpG + fpM + fpRrh + fpUrh
-  
-  # ---- Next count the number of restrictions -------- %
-    rDB <- size(opt$R_psi,1)
-  
-  
-  if(isempty(opt$R_Beta) && isempty(opt$R_Alpha))
-    # If Alpha or Beta are unrestricted, only an identification restriction
-  #   is imposed
-  rB <- r*r # identification restrictions (eye(r))
-  numRest <- rDB + rB
-  # ------ Free parameters is the difference between the two ---- %
-    fp <- numParams - numRest
-  else
-    # If there are restrictions imposed on beta or alpha, we can use the rank
-  #   condition calculated in the main estimation function to give us the
-  #   number of free parameters in alpha, beta, and the restricted constant
-  #   if it is being estimated:
-    fpAlphaBetaRhoR <- rankJ
-  fp <- fpAlphaBetaRhoR + (fDB + fpG + fpM + fpUrh) - rDB    
-  end
-  
-  end
-  
+
+# Set dimensions of matrices.
+p <- size(x, 2)
+
+# rhoHat and betaHat are not used in the Hessian calculation.
+rho <- coeffs$rhoHat
+beta <- coeffs$betaHat
+
+# Specify delta (increment for numerical derivatives).
+delta <- 10^(-4)
+
+# Convert the parameters to vector form.
+phi0 <- SEmat2vecU(coeffs, k, r, p, opt)
+
+# Calculate vector of increments.
+deltaPhi <- delta*ones(size(phi0))
+
+nPhi <- length(phi0)
+
+# Initialize the Hessian matrix.
+hessian <- zeros(nPhi)
+
+# The loops below evaluate the likelihood at second order incremental
+#   shifts in the parameters. 
+
+for i <- 1:nPhi
+for j <- 1:i
+
+# positive shift in both parameters.
+phi1 <- phi0 + deltaPhi.*( (1:nPhi) == i ) + deltaPhi.*( (1:nPhi) == j )
+[ coeffsAdj ] <- SEvec2matU( phi1, k, r, p, opt )
+# calculate likelihood
+like1 <- FullFCVARlike(x, k, r, coeffsAdj, beta, rho, opt )
+
+# negative shift in first parameter, positive shift in second
+# parameter. If same parameter, no shift.
+phi2 <- phi0 - deltaPhi.*( (1:nPhi) == i ) + deltaPhi.*( (1:nPhi) == j )
+[ coeffsAdj ] <- SEvec2matU( phi2, k, r, p, opt )
+# calculate likelihood
+like2 <- FullFCVARlike(x, k, r, coeffsAdj, beta, rho, opt)
+
+# positive shift in first parameter, negative shift in second
+# parameter. If same parameter, no shift.
+phi3 <- phi0 + deltaPhi.*( (1:nPhi) == i ) - deltaPhi.*( (1:nPhi) == j )
+[ coeffsAdj ] <- SEvec2matU( phi3, k, r, p, opt )
+# calculate likelihood
+like3 <- FullFCVARlike(x, k, r, coeffsAdj, beta, rho, opt)
+
+# negative shift in both parameters.
+phi4 <- phi0 - deltaPhi.*( (1:nPhi) == i ) - deltaPhi.*( (1:nPhi) == j )
+[ coeffsAdj ] <- SEvec2matU( phi4, k, r, p, opt )
+# calculate likelihood
+like4 <- FullFCVARlike(x, k, r, coeffsAdj, beta, rho, opt)
+
+# The numerical approximation to the second derivative.
+hessian(i,j) <- ( like1 - like2 - like3 + like4 )/4/deltaPhi(i)/deltaPhi(j)
+
+# Hessian is symmetric.
+hessian(j,i) <- hessian(i,j)
+
+end
+end
+end
