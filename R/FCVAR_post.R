d36c997f62f61a6de653fd2ada5bd5bb50e19dbb
Lee Morin <Lealand.Morin@ucf.edu>
2020-04-03 23:41:11 -0400

    Function and description for white noise tests

--- R/FCVAR_post.R
+++ R/FCVAR_post.R
@@ -127,7 +127,7 @@ mv_wntest <- function(x, maxlag, printResults) {
 #' from the estimation results of \code{FCVARestn}.
 #' An alternative test is the Ljung-Box Q-test in \code{Qtest}.
 #' @note
-#' The LM test should be consistent for heteroskedastic series.
+#' The LM test is consistent for heteroskedastic series.
 #'
 LMtest <- function(x,q) {
 
@@ -213,4 +213,92 @@ LMtest <- function(x,q) {
 
   return(LMtest_out)
 }
+
+
+#' Ljung-Box Q-test for Serial Correlation
+#'
+#' \code{Qtest} performs a (multivariate) Ljung-Box Q-test for serial correlation, see
+# 	Luetkepohl (2005, New Introduction to Multiple Time Series Analysis, p. 169).
+#'
+#' @param x A vector or Tx1 matrix of variables to be tested,
+#' typically model residuals.
+#' @param maxlag The number of lags for the serial correlation tests.
+#' @return A list object \code{LMtest_out} containing the test results,
+#' including the following parameters:
+#' \describe{
+#'   \item{\code{Qstat}}{A 1xp vector of Q statistics for individual series.}
+#'   \item{\code{pv}}{A 1xp vector of P-values for Q-test on individual series.}
+#' }
+#' @examples
+#' Qtest(x = rnorm(100), maxlag = 10)
+#'
+#' Qtest(x = cumsum(rnorm(100)), maxlag = 10)
+#' @family FCVAR postestimation functions
+#' @seealso \code{mv_wntest} calls this function to test residuals
+#' from the estimation results of \code{FCVARestn}.
+#' An alternative test is the Breusch-Godfrey Lagrange Multiplier Test in \code{LMtest}.
+#' @note
+#' The LM test in \code{LMtest} is consistent for heteroskedastic series,
+#' while the Q-test is not.
+#' @references H. Luetkepohl (2005) "New Introduction to Multiple Time Series Analysis," Springer, Berlin.
+#'
+Qtest <- function(x, maxlag) {
+
+  # print('class(x) = ')
+  # print(class(x))
+
+  T <- nrow(x)
+  p <- ncol(x)
+
+  # print('p = ')
+  # print(p)
+  # print('T = ')
+  # print(T)
+
+  # t <- 7
+  # print(x[t, , drop = FALSE])
+  # print(t(x[t, , drop = FALSE]))
+  # print(t(x[t, , drop = FALSE]) %*% x[t, , drop = FALSE])
+
+  C0 <- matrix(0, nrow = p, ncol = p)
+  for (t in 1:T) {
+    # C0 <- C0 + x[t, ] %*% t(x[t, ])
+    C0 <- C0 + t(x[t, , drop = FALSE]) %*% x[t, , drop = FALSE]
+  }
+  C0 <- C0/T
+
+  C <- array(rep(0, p*p*maxlag), dim = c(p,p,maxlag))
+  for (i in 1:maxlag) {
+    for (t in (i+1):T) {
+      C[ , ,i] <- C[ , ,i] + t(x[t, , drop = FALSE]) %*% x[t-i, , drop = FALSE]
+    }
+
+    C[ , ,i] <- C[ , ,i]/(T-i) # Note division by (T-i) instead of T.
+  }
+
+
+  # (Multivariate) Q statistic
+  Qstat <- 0
+  for (j in 1:maxlag) {
+    # Qstat <- Qstat+trace(C(:,:,j)'*inv(C0)*C(:,:,j)*inv(C0)) / (T-j) %'
+    # The following line is a more efficient calculation than the previous
+    # Qstat <- Qstat+trace( (C(:,:,j)'/C0)*(C(:,:,j)/C0) ) / (T-j) %' #'
+    # Need function for trace:
+    Qstat <- Qstat + sum(diag( (t(C[ , ,j]) %*% solve(C0)) %*% (C[ , ,j] %*% solve(C0)) )) / (T-j)
+  }
+
+
+  Qstat <- Qstat*T*(T+2)
+  pv <- 1 - pchisq(Qstat, p*p*maxlag) # P-value is calculated with p^2*maxlag df.
+
+
+  # Output a list of results.
+  Qtest_out <- list(
+    Qstat = Qstat,
+    pv = pv
+  )
+
+  return(Qtest_out)
+}
+
 
